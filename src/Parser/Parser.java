b5f2bb34e39cf7b8e7dd98717778ff3f243ff30a
MMG76 <m.gorji76@yahoo.com>
2019-05-20 03:14:19 +0430

    Add initializeSet to Parser

--- src/Parser/Parser.java
+++ src/Parser/Parser.java
@@ -6,8 +6,10 @@ import java.util.*;
 
 public class Parser {
     private static Map<String, Diagram> diagrams;
+    private static Map<String, List<String>> first;
+    private static Map<String, List<String>> follow;
 
-    private static void initialize() throws FileNotFoundException {
+    private static void initializeDiagram() throws FileNotFoundException {
         diagrams = new HashMap<>();
 
         Scanner grammarScanner = new Scanner(new File("grammar.txt"));
@@ -24,18 +26,45 @@ public class Parser {
         }
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
-        initialize();
+    private static void initializeSet(Map<String, List<String>> set, String pathname) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(pathname));
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] values = line.split(" ");
+            List<String> setValues = new ArrayList<>();
 
-        Diagram diagram = diagrams.get("DEFAULT_STMT");
-        Queue<DiagramNode> q = new LinkedList<>();
-        q.add(diagram.start);
-        while (!q.isEmpty()) {
-            for (DiagramEdge edge : q.peek().edges) {
-                q.add(edge.nextNode);
-                System.out.println(q.peek().hashCode() + " " + edge.label + " " + edge.nextNode.hashCode());
-            }
-            q.remove();
+            for (int i = 1; i < values.length; i++)
+                if (values[i].equals("."))
+                    setValues.add("");
+                else
+                    setValues.add(values[i]);
+
+            set.put(values[0], setValues);
         }
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        initializeDiagram();
+
+        first = new HashMap<>();
+        follow = new HashMap<>();
+
+        // First and follow are derived from http://smlweb.cpsc.ucalgary.ca
+        initializeSet(first, "first.txt");
+        initializeSet(follow, "follow.txt");
+
+//        for (String firstValue : first.get("EXPRESSION_STMT"))
+//            System.out.println(firstValue);
+
+//        Diagram diagram = diagrams.get("DEFAULT_STMT");
+//        Queue<DiagramNode> q = new LinkedList<>();
+//        q.add(diagram.start);
+//        while (!q.isEmpty()) {
+//            for (DiagramEdge edge : q.peek().edges) {
+//                q.add(edge.nextNode);
+//                System.out.println(q.peek().hashCode() + " " + edge.label + " " + edge.nextNode.hashCode());
+//            }
+//            q.remove();
+//        }
     }
 }
