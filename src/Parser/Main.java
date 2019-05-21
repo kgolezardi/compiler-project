1188595f6aa810a65e1fcbce402492e3f71832bb
Kiarash Golezardi <golezardi@ce.sharif.edu>
2019-05-21 16:45:31 +0430

    Remove useless code from parser main

--- src/Parser/Main.java
+++ src/Parser/Main.java
@@ -5,33 +5,9 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.printf("%-25s : %25s%n\n", "left justified", "right justified");
-        System.out.printf("%25s : %-25s%n\n", "right justified", "left justified");
-
         Parser parser = new Parser("code.txt");
         ParseTreeNode parseTree = parser.parse();
-//        ParseTreeNode a = new ParseTreeNode("A");
-//        ParseTreeNode b = new ParseTreeNode("B");
-//        ParseTreeNode c = new ParseTreeNode("C");
-//        ParseTreeNode d = new ParseTreeNode("D");
-//        ParseTreeNode e = new ParseTreeNode("E");
-//        ParseTreeNode f = new ParseTreeNode("F");
-//        ParseTreeNode g = new ParseTreeNode("G");
-//        ParseTreeNode h = new ParseTreeNode("H");
-//        ParseTreeNode i = new ParseTreeNode("I");
-//        ParseTreeNode j = new ParseTreeNode("J");
-//        ParseTreeNode k = new ParseTreeNode("K");
-//
-//        a.children.add(b);
-//        a.children.add(c);
-//        b.children.add(d);
-//        d.children.add(g);
-//        d.children.add(h);
-//        c.children.add(e);
-//        c.children.add(f);
-//        f.children.add(i);
-//        f.children.add(j);
-//        f.children.add(k);
+
         FileWriter parseTreeFile = new FileWriter("parse-tree.txt");
         printTree(parseTreeFile, parseTree, 0);
         parseTreeFile.close();
